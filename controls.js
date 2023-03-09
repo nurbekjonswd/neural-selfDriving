d51eb6e7b5886a0cec907a30dbf2013037b0a734
cleaverprogrammer <nurbek.malikov199711@gmail.com>
2023-03-09 12:46:10 +0500

    controls

--- controls.js
+++ controls.js
@@ -8,7 +8,39 @@ class Controls {
     this.#addKeybordListeners();
   }
 
-  #addKeybordListener() {
-    document.onkeydown = (event) => {};
+  #addKeybordListeners() {
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
+      }
+    };
+
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+      }
+    };
   }
 }
